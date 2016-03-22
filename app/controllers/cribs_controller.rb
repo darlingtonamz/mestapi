3a8515c02dc3a9975dc6e2f32cee0614afc6d627
Amanze Ogbonna <darlingtonamz@gmail.com>
2016-03-22 16:06:41 +0000

    Changed FS and corrected http status code

--- app/controllers/cribs_controller.rb
+++ app/controllers/cribs_controller.rb
@@ -4,8 +4,25 @@ class CribsController < ApplicationController
 	end
 
 	def show
-		@crib = Crib.find(params[:id])
-		@owner = @crib.owner
-		@tenants = @crib.tenants
+		@crib = Crib.find_by_id(params[:id])
+		if (@crib)
+			@owner = @crib.owner
+			@tenants = @crib.tenants
+		else
+			@error = {
+				code: 815,
+				message: "The Crib with id #{params[:id]} doesn't exist"
+			}
+			render json: "error", status: 404
+		end
+
+		respond_to do |format|
+			format.json
+		end
 	end
+
+	def create
+
+	end
+
 end
